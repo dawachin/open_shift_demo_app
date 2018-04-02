fbb7c05048ab0c004eedcff639e9bb494aa922ba
dawachin <ohwada.hiroaki@jp.fujitsu.com>
2018-04-02 19:36:22 +0900

    mysql対応

--- src/main/java/com/example/open_shift_demo_app/model/entity/EventEntity.java
+++ src/main/java/com/example/open_shift_demo_app/model/entity/EventEntity.java
@@ -1,15 +1,13 @@
 package com.example.open_shift_demo_app.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Value;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 
 /**
@@ -18,7 +16,7 @@ import java.util.Date;
  *
  * */
 @AllArgsConstructor
-@Document
+@Entity
 public class EventEntity {
 
     public EventEntity(){};
@@ -26,22 +24,32 @@ public class EventEntity {
     /** スケジュールID */
     @Id
     @Getter
+    @Setter
+    @Column(name="ID")
     private String id;
 
     /** スケジュール　タイトル */
     @Getter
+    @Setter
+    @Column(name="TITLE")
     private String title;
 
     /** スケジュール　説明 */
     @Getter
+    @Setter
+    @Column(name="DESCRIPTION")
     private String description;
 
     /** スケジュール　開始日時 */
     @Getter
+    @Setter
+    @Column(name="START_DATE")
     private Date startDate;
 
     /** スケジュール　終了日時 */
     @Getter
+    @Setter
+    @Column(name="END_DATE")
     private Date endDate;
 
 }
--- src/main/java/com/example/open_shift_demo_app/model/repository/EventRepository.java
+++ src/main/java/com/example/open_shift_demo_app/model/repository/EventRepository.java
@@ -1,15 +1,12 @@
 package com.example.open_shift_demo_app.model.repository;
 
 import com.example.open_shift_demo_app.model.entity.EventEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 
-public interface EventRepository extends MongoRepository<EventEntity, String> {
-
-    @Override
-    Iterable<EventEntity> findAllById(Iterable<String> iterable);
+public interface EventRepository extends JpaRepository<EventEntity, String> {
 
     Optional<EventEntity> findEventEntityById(String eventId);
 
